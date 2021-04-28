8bae319f497d23b8ecea8ce878428f60aa61aefd
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-04-28 17:48:28 +0200

    nanofix

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -4,7 +4,7 @@ const BookList = props => {
     let result = ""
 
     try {
-        result = props.books.map(book => <SingleBook book={book} />)
+        result = props.books.map(book => <SingleBook book={book} key={book.asin} />)
     } catch (error) {
         console.log("oh no error, beep baap boop")
         return null
