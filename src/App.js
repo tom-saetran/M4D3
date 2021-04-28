686bfe666c14aa750f157961503bb08d1344df81
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-04-28 16:26:24 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import WarningSign from "./components/WarningSign"
 import MyBadge from "./components/MyBadge"
-import SingleBook from "./components/SingleBook"
-
 import FilterBookList from "./components/FilterBookList"
-
-import FantasyBooks from "./data/fantasy.json"
 
 const App = () => (
     <>
         <FilterBookList />
         <WarningSign text="ERROR OH NO" />
         <MyBadge variant="warning" text="Something cool" />
-        <SingleBook book={FantasyBooks[0]} />
     </>
 )
 
